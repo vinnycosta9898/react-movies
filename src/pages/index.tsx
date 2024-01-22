ecc91d3f6fd3a142e9b01ab9984252f26fed6d49
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-01-22 20:00:00 -0300

    Criando a tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
+import { LogosGoogleIcon } from "@/components/Icons/Google";
+import { LogosReact } from "@/components/Icons/React";
+
 export default function Home() {
   return (
-    <h1 className="text-xl">Hello world</h1>
+    <div className="w-screen h-screen bg-black flex">
+      <aside className="w-[50%] h-full">
+  
+      </aside>
+
+      <div className="flex-1 h-full text-2xl text-white flex flex-col items-center">
+        <div className="w-62 h-44 flex items-center justify-around mt-12">
+          <h1 className="text-2xl mx-4">React Movies</h1>
+          <LogosReact width={60}/>
+        </div>
+
+        <button className="w-64 h-12 bg-gray-800 flex justify-around items-center rounded-lg">
+          <LogosGoogleIcon/>
+          Entrar com Google
+        </button>
+        
+      </div>
+    </div>
   );
 }
