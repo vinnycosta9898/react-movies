91f5cb98783385bd55770414d6cff795f69b7162
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-02-06 09:46:35 -0300

    Removendo importacacoes do google

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,8 @@
 import colors from "tailwindcss/colors";
-import Image from "next/image";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
 import { FaReact } from "react-icons/fa"
 
 export function Header() {
-  const session = useSession();
-  const statusAuthenticated = session.status
-  const username = session.data?.user.name
-
   return (
     <div className="min-w-screen h-[6rem] flex items-center justify-around sticky px-8">
       <div className="h-full flex items-center">
@@ -31,26 +25,6 @@ export function Header() {
           Meu Filmes
         </Link>
       </nav>
-
-      <Link href="/profile" className="flex items-center justify-center">
-        <h1 className="text-white text-lg font-bold mx-2">{username}</h1>
-        {statusAuthenticated === "unauthenticated" && (
-          <Link href="/" className="text-white text-lg font-bold mx-2">
-            Fazer login
-          </Link>
-        )}
-        {session.status === "unauthenticated" ? (
-          <div className="w-10 h-10 bg-white rounded-[999px] md:text-xl sm:text-lg xsm:w-8 h-8"></div>
-        ) : (
-          <Image
-            src={session.data?.user.avatar_url as string}
-            alt={session.data?.user.name as string}
-            width={80}
-            height={80}
-            className="w-10 h-10 bg-white rounded-[999px]"
-          />
-        )}
-      </Link>
     </div>
   );
 }
